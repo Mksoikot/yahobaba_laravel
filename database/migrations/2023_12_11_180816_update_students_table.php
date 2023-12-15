8ce82bd5f53f25c28f904a1a9581928b2de0210f
Mksoikot <mksoikotbhuiyan117@gmail.com>
2023-12-15 10:24:50 +0600

    migration practice

--- database/migrations/2023_12_11_180816_update_students_table.php
+++ database/migrations/2023_12_11_180816_update_students_table.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::table('students', function (Blueprint $table) {
+            // $table->renameColumn('city','cities');
+            $table->dropColumn('percentage');
+            $table->string('city',155)->default('no_city')->change();
         });
     }
 
@@ -23,6 +23,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::table('students', function (Blueprint $table) {
+            //
+        });
     }
 };
