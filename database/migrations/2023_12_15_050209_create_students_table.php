34f4f0e7dbc049020cea18dfb1102dbd3d8063b9
Mksoikot <mksoikotbhuiyan117@gmail.com>
2023-12-15 12:53:37 +0600

    migration prctrice done

--- database/migrations/2023_12_15_050209_create_students_table.php
+++ database/migrations/2023_12_15_050209_create_students_table.php
@@ -12,13 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('students', function (Blueprint $table) {
-            $table->integer('student_id');
+            $table->id();
             $table->string('name',100);
-            $table->string('city',50)->default('No City');
             $table->string('email',50)->unique()->nullable();
-            $table->float('percentage',3,2)->comment("Student Percentage");
-            $table->primary('student_id');
-            $table->integer('age')->unsigned();
             $table->timestamps();
         });
     }
